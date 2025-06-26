8d8918735729e579c780557882c479031b75cbd9
andydodo <andy415do@gmail.com>
2025-06-26 05:17:53 -0400

    Site updated: 2025-06-26 05:17:53

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,10 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://s4.ax1x.com/2021/12/30/' + data.link[i];
-          var src = 'https://s4.ax1x.com/2021/12/30/' + data.link[i];
+          // var minSrc = 'https://s4.ax1x.com/2021/12/30/' + data.link[i];
+          // var src = 'https://s4.ax1x.com/2021/12/30/' + data.link[i];
+	  var minSrc = 'photos/minins/' + data.link[i] + '.min.jpg';
+	  var src = 'photos/ins/' + data.link[i] + '.jpg';
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
 	  var size = data.size[i];
