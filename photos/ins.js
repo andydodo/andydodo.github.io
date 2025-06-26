304b1ae95744ef9a581268723d2d4540bed2e986
andydodo <andy415do@gmail.com>
2025-06-26 05:51:36 -0400

    Site updated: 2025-06-26 05:51:36

--- photos/ins.js
+++ photos/ins.js
@@ -116,25 +116,25 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-	  var minSrc = './minins/' + data.link[i] + '.min.jpg';
-	  var src = './ins/' + data.link[i] + '.jpg';
-          var type = data.type[i];
-          var target = src + (type === 'video' ? '.mp4' : '.jpg');
-	  var size = data.size[i];
-          src += '';
+          var minSrc = './minins/' + data.link[i] + '.min.jpg';
+          var src = './ins/' + data.link[i];
+          var type = 'images';
+	  var sizes = data.sizes[i];
+	  src += '.jpg';
+          var target = src;
 
-          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                <a href="' + src + '" itemprop="contentUrl" data-size="' + size + '" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="">\
+                <a href="' + src + '" data-size="' + sizes + '" data-type="' + type + '" data-target="' + target + '">\
+                  <img class="reward-img" data-type="' + type + '" src="' + minSrc + '" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
         }
-        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
+        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '<em>' + data.month + '月</em></h1>\
         <ul class="img-box-ul">' + liTmpl + '</ul>\
         </section>';
       }
-      document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
+      document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="">' + ulTmpl + '</div>';
       createVideoIncon();
       _view2.default.init();
     };
